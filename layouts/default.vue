8daf7d2a131ec9842b10f37fbaaaf80233af4b44
pooriavakili <pooriavakili09@gmail.com>
2022-06-14 23:12:00 +0430

    portfolio-nuxt

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,31 +2,130 @@
   <v-app dark>
 
     <v-app-bar fixed app>
-      <v-btn to="/" plain>login</v-btn>
-      <v-btn to="/about" plain>about</v-btn>
-      <v-btn to="/service" plain>services</v-btn>
-      <v-btn to="/price" plain>price</v-btn>
-      <v-btn to="/blog" plain>blog</v-btn>
-
-
+      <nuxt-link class="link" to="/">login</nuxt-link>
+      <nuxt-link class="ml-4 link" to="/about">about</nuxt-link>
+      <nuxt-link to="/service" class="ml-4 link">services</nuxt-link>
+      <nuxt-link to="/price" class="ml-4 link">price</nuxt-link>
+      <nuxt-link to="/blog" class="ml-4 link">blog</nuxt-link>
       <v-spacer />
-
     </v-app-bar>
     <v-main class="white">
-
       <Nuxt />
-
     </v-main>
+    <v-footer class="mt-5" dark padless>
+      <v-flex xs12 md12 sm10>
+        <v-card width="1400" height="300" flat tile class="black cart-one  white--text text-center">
+          <v-card-actions>
+            <v-container>
+              <v-row>
+                <div>
+                  <p>PetLover</p>
+                  <v-row>
+                    Ipsum amet sed vero et lorem stet eos ut, labore
+                  </v-row>
+                  <v-row>
+                    sed sed stet sea est ipsum ut. Volup amet ea
+                  </v-row>
+                  <v-row>
+                    sanct ipsum, dolore vero lorem no duo eirmod.
+                  </v-row>
 
-    <v-footer app>
+                </div>
 
-      <Footer />
+                <v-spacer />
+                <div class="get">
+                  <p>Get In Touch</p>
+                  <div>
+                    <v-row class="street">
+                      <fa class="mt-1" :icon="['fa', 'location-dot']" />
+                      <div class="ml-3 ">123 Street, New York, USA</div>
+                    </v-row>
+                    <v-row class="street">
+                      <fa class="mt-1" :icon="['fa', 'phone']" />
+                      <div class="ml-3">+012 345 67890</div>
+                    </v-row>
+                    <v-row class="street">
+                      <fa class="mt-1" :icon="['fa', 'envelope']" />
+                      <div class="ml-3">mail@domain.com</div>
+                    </v-row>
+                    <v-row class="street mt-3">
+                      <v-btn color="black" small fab>
+                        <v-icon>
+                          mdi-instagram
+                        </v-icon>
+                      </v-btn>
+                      <v-btn color="black" small fab>
+                        <v-icon>
+                          mdi-linkedin
+                        </v-icon>
+                      </v-btn>
+                      <v-btn color="black" small fab>
+                        <v-icon>
+                          mdi-facebook
+                        </v-icon>
+                      </v-btn>
+                      <v-btn color="black" small fab>
+                        <v-icon>
+                          mdi-twitter
+                        </v-icon>
+                      </v-btn>
+                    </v-row>
+                  </div>
+                </div>
+                <v-spacer />
+                <div class="pop">
+                  <p>Popular Links</p>
+                  <nuxt-link class="link" to="/">
+                    <v-row class="ml-4">
 
+                      <fa :icon="['fa', 'lock']" />
+                      <div class="">login</div>
+                    </v-row>
+                  </nuxt-link>
+                  <nuxt-link class="link " to="/about">
+                    <div class="about">
+                      <v-row class="ml-4 mt-2  ">
 
+                        <fa :icon="['fa', 'user']" />
+                        <div>about</div>
+                      </v-row>
+                    </div>
+                  </nuxt-link>
+                  <nuxt-link class="link" to="/service">
+                    <div class="about">
+                      <v-row class="ml-4 mt-2 price">
+
+                        <fa :icon="['fa', 'user']" />
+                        <div class="">service</div>
+                      </v-row>
+                    </div>
+                  </nuxt-link>
+                  <nuxt-link class="link" to="/price">
+                    <div class="about">
+                      <v-row class="ml-4 mt-2 price">
+                        <fa class="" :icon="['fa', 'user']" />
+                        <div class="">price</div>
+                      </v-row>
+                    </div>
+                  </nuxt-link>
+                  <nuxt-link class="link" to="/price">
+                    <div class="about">
+                      <v-row class="ml-4 mt-2 price">
+
+                        <fa class="" :icon="['fa', 'user']" />
+                        <div class="">blog</div>
+                      </v-row>
+                    </div>
+                  </nuxt-link>
+                </div>
+              </v-row>
+            </v-container>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-footer>
   </v-app>
 </template>
-
 <script>
 import Footer from "../pages/footer.vue"
 export default {
@@ -34,12 +133,36 @@ export default {
   components: {
     Footer
   },
-  data() {
-    return {
-
-    }
-  }
 }
 </script>
 <style scoped>
+.link {
+  text-decoration: none;
+  color: #fff;
+  cursor: pointer
+}
+
+@media screen and (max-width: 800px) {
+  .pop {
+    margin-top: 20px
+  }
+
+  .get {
+    margin-left: 40px
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .get {
+    margin-top: 20px
+  }
+
+  .street {
+    margin-left: 15px
+  }
+
+  .about {
+    margin-top: 15px
+  }
+}
 </style>
--- pages/footer.vue
+++ pages/footer.vue
@@ -1,23 +1,57 @@
 <template>
-  <v-container>
+  <v-card height="400px">
+    <v-footer v-bind="localAttrs" :padless="padless">
+      <v-card flat tile width="100%" class="red lighten-1 text-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
 
-    <v-col cols="12">
+        <v-divider></v-divider>
 
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
 
-      <v-row>
-        <div>home</div>
-        <v-spacer />
-        <div>home</div>
-        <v-spacer />
-        <div>home</div>
-      </v-row>
-    </v-col>
-  </v-container>
+  </v-card>
 </template>
 
 <script>
-export default {
 
+export default {
+  data: () => ({
+    icons: [
+      'mdi-home',
+      'mdi-email',
+      'mdi-calendar',
+      'mdi-delete',
+    ],
+    items: [
+      'default',
+      'absolute',
+      'fixed',
+    ],
+    padless: false,
+    variant: 'default',
+  }),
+  computed: {
+    localAttrs() {
+      const attrs = {}
+
+      if (this.variant === 'default') {
+        attrs.absolute = false
+        attrs.fixed = false
+      } else {
+        attrs[this.variant] = true
+      }
+      return attrs
+    },
+  },
 }
 </script>
 
